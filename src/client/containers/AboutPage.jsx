b67c687dbb1d8f20aa7790be3134d80b619b5ec3
Thang Thai <thaithangt@gmail.com>
2022-09-21 15:50:06 -0700

    Feat: Updating styles and adding content on About page

--- src/client/containers/AboutPage.jsx
+++ src/client/containers/AboutPage.jsx
@@ -1,101 +1,158 @@
 import React from 'react';
 
-
 const AboutPage = () => {
   return (
-    <div id='landing-container'>
-      <h1>I am the landing page</h1>
-      <section id='intro'>
-        <figure className='div-side'>
+    <div id="landing-container">
+      <section id="hero">
+        <figure className="div-side">
           <img src="public/logo.png" alt="" />
         </figure>
-        <main className='div-main'>
-          <p>This is the blah blah part about what Palaemon is</p>
+        <main className="div-main">
+          <h1>Introducing Palaemon</h1>
+          <p className="hero-subheading">
+            Open-source developer tool for monitoring Kubernetes clusters and
+            error analysis
+          </p>
+          <ul className="hero-ul">
+            <li className="hero-li-1">Easy and straightforward</li>
+            <li className="hero-li-2">
+              Our documentation makes setting up quick and easy
+            </li>
+            <li className="hero-li-1">Addressing the Gaps</li>
+            <li className="hero-li-2">
+              Palaemon aims to address Kubernetes' lack of native monitoring and
+              robust error tracking
+            </li>
+            <li className="hero-li-1">Open-source</li>
+            <li className="hero-li-2">
+              Palaemon highly encourages contributors and suggestions for
+              improvement
+            </li>
+          </ul>
         </main>
       </section>
-      <section id='features'>
-        <main className='div-main'>
+      <section id="features">
+        <main className="div-main">
           <p>This div will showcase our awesome features!!</p>
         </main>
-        <figure className='div-side'>
+        <figure className="div-side">
           <img src="https://kubernetes.io/images/favicon.png" alt="" />
         </figure>
       </section>
-      <section id='demo'>
-        <main className='div-main'>
+      <section id="demo">
+        <main className="div-main">
           <p>We'll show a cool gif demo-ing our app here!!</p>
         </main>
-        <figure className='div-side'>
+        <figure className="div-side">
           <img src="public/logo.png" alt="" />
         </figure>
       </section>
-      <section id='contact'>
-        <p>This is where we'll have our beautiful photos and ways to connect with such spectacular engineers like us!</p>
-        <div className='spectacular'>
-          <div className='spectacular-person'>
-            <div className='spectacular-person-photo'> <img src="public/logo.png" alt="" /></div>
-            <div className='spectacular-person-name'>NAME</div>
-            <div className='spectacular-person-title'>title</div>
-            <div className='spectacular-person-links'>
-              <div className='spectacular-person-github'>
+      <section id="contact">
+        <p>
+          This is where we'll have our beautiful photos and ways to connect with
+          such spectacular engineers like us!
+        </p>
+        <div className="spectacular">
+          <div className="spectacular-person">
+            <div className="spectacular-person-photo">
+              {' '}
+              <img src="public/logo.png" alt="" />
+            </div>
+            <div className="spectacular-person-name">NAME</div>
+            <div className="spectacular-person-title">title</div>
+            <div className="spectacular-person-links">
+              <div className="spectacular-person-github">
                 <a href="https://github.com/oslabs-beta/Palaemon">
-                  <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" />
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                    alt=""
+                  />
                 </a>
               </div>
-              <div className='spectacular-person-linkedin'>
+              <div className="spectacular-person-linkedin">
                 <a href="https://github.com/oslabs-beta/Palaemon">
-                  <img src="http://cdn.onlinewebfonts.com/svg/img_24651.png" alt="" />
+                  <img
+                    src="http://cdn.onlinewebfonts.com/svg/img_24651.png"
+                    alt=""
+                  />
                 </a>
               </div>
             </div>
           </div>
-          <div className='spectacular-person'>
-            <div className='spectacular-person-photo'> <img src="public/logo.png" alt="" /></div>
-            <div className='spectacular-person-name'>NAME</div>
-            <div className='spectacular-person-title'>title</div>
-            <div className='spectacular-person-links'>
-              <div className='spectacular-person-github'>
+          <div className="spectacular-person">
+            <div className="spectacular-person-photo">
+              {' '}
+              <img src="public/logo.png" alt="" />
+            </div>
+            <div className="spectacular-person-name">NAME</div>
+            <div className="spectacular-person-title">title</div>
+            <div className="spectacular-person-links">
+              <div className="spectacular-person-github">
                 <a href="https://github.com/oslabs-beta/Palaemon">
-                  <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" />
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                    alt=""
+                  />
                 </a>
               </div>
-              <div className='spectacular-person-linkedin'>
+              <div className="spectacular-person-linkedin">
                 <a href="https://github.com/oslabs-beta/Palaemon">
-                  <img src="http://cdn.onlinewebfonts.com/svg/img_24651.png" alt="" />
+                  <img
+                    src="http://cdn.onlinewebfonts.com/svg/img_24651.png"
+                    alt=""
+                  />
                 </a>
               </div>
             </div>
           </div>
-          <div className='spectacular-person'>
-            <div className='spectacular-person-photo'> <img src="public/logo.png" alt="" /></div>
-            <div className='spectacular-person-name'>NAME</div>
-            <div className='spectacular-person-title'>title</div>
-            <div className='spectacular-person-links'>
-              <div className='spectacular-person-github'>
+          <div className="spectacular-person">
+            <div className="spectacular-person-photo">
+              {' '}
+              <img src="public/logo.png" alt="" />
+            </div>
+            <div className="spectacular-person-name">NAME</div>
+            <div className="spectacular-person-title">title</div>
+            <div className="spectacular-person-links">
+              <div className="spectacular-person-github">
                 <a href="https://github.com/oslabs-beta/Palaemon">
-                  <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" />
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                    alt=""
+                  />
                 </a>
               </div>
-              <div className='spectacular-person-linkedin'>
+              <div className="spectacular-person-linkedin">
                 <a href="https://github.com/oslabs-beta/Palaemon">
-                  <img src="http://cdn.onlinewebfonts.com/svg/img_24651.png" alt="" />
+                  <img
+                    src="http://cdn.onlinewebfonts.com/svg/img_24651.png"
+                    alt=""
+                  />
                 </a>
               </div>
             </div>
           </div>
-          <div className='spectacular-person'>
-            <div className='spectacular-person-photo'> <img src="public/logo.png" alt="" /></div>
-            <div className='spectacular-person-name'>NAME</div>
-            <div className='spectacular-person-title'>title</div>
-            <div className='spectacular-person-links'>
-              <div className='spectacular-person-github'>
+          <div className="spectacular-person">
+            <div className="spectacular-person-photo">
+              {' '}
+              <img src="public/logo.png" alt="" />
+            </div>
+            <div className="spectacular-person-name">NAME</div>
+            <div className="spectacular-person-title">title</div>
+            <div className="spectacular-person-links">
+              <div className="spectacular-person-github">
                 <a href="https://github.com/oslabs-beta/Palaemon">
-                  <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" />
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                    alt=""
+                  />
                 </a>
               </div>
-              <div className='spectacular-person-linkedin'>
+              <div className="spectacular-person-linkedin">
                 <a href="https://github.com/oslabs-beta/Palaemon">
-                  <img src="http://cdn.onlinewebfonts.com/svg/img_24651.png" alt="" />
+                  <img
+                    src="http://cdn.onlinewebfonts.com/svg/img_24651.png"
+                    alt=""
+                  />
                 </a>
               </div>
             </div>
--- src/client/containers/Navbar.jsx
+++ src/client/containers/Navbar.jsx
@@ -1,14 +1,26 @@
 const Navbar = () => {
   return (
     <nav className="nav" id="navbar">
-      <a href="/"><img className="icon" id="logo" src="public/logo-hat.png" /></a>
-      <div className="nav-right-container">
-        <ul className="nav-links">
-          <li><a href='/'>About</a></li>
-          <li><a href='/docs'>Docs</a></li>
-          <li><a href="https://github.com/oslabs-beta/Palaemon">Github</a></li>
-          <li><a href="/medium">Blog</a></li>
-        </ul>
+      <div className="nav-container">
+        <a href="/">
+          <img className="icon" id="logo" src="public/logo-hat.png" />
+        </a>
+        <div className="nav-right-container">
+          <ul className="nav-links">
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/docs">Docs</a>
+            </li>
+            <li>
+              <a href="https://github.com/oslabs-beta/Palaemon">Github</a>
+            </li>
+            <li>
+              <a href="/medium">Blog</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
