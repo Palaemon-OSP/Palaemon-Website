9927f667e86d07c667fe813e56008ff90802d0b8
Si Young Mah <siyoungmah@gmail.com>
2022-09-20 23:48:56 -0700

    Add basic styling for 404 error page

--- src/client/containers/NotFoundPage.jsx
+++ src/client/containers/NotFoundPage.jsx
@@ -6,6 +6,7 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
+      <img src="logo.png" alt="" />
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
