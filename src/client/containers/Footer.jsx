8efced3d6adabf6592430f12f17544e627ca4ef6
Si Young Mah <siyoungmah@gmail.com>
2022-09-20 23:45:32 -0700

    Add basic styling to AboutPage

--- src/client/containers/Footer.jsx
+++ src/client/containers/Footer.jsx
@@ -8,8 +8,12 @@ const Footer = () => {
         <h1 id="copyright">&copy; 2022 Palaemon | MIT License</h1>
       </div>
       <div className="footer-right">
-        <img className="footer-logos" src="https://cdn-icons-png.flaticon.com/512/2111/2111432.png" alt="" />
-        <img className="footer-logos" src="https://cdn-icons-png.flaticon.com/512/5968/5968933.png" alt="" />
+        <a href="https://github.com/oslabs-beta/Palaemon">
+          <img className="footer-logos" src="https://cdn-icons-png.flaticon.com/512/2111/2111432.png" alt="" />
+        </a>
+        <a href="https://github.com/oslabs-beta/Palaemon">
+          <img className="footer-logos" src="https://cdn-icons-png.flaticon.com/512/5968/5968933.png" alt="" />
+        </a>
       </div>
     </footer>
   );
