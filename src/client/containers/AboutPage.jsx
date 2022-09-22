31d0604aeb78eb2ee621b1daea6dc50913aab41b
Thang Thai <thaithangt@gmail.com>
2022-09-21 21:27:29 -0700

    About and Docs updated

--- src/client/containers/AboutPage.jsx
+++ src/client/containers/AboutPage.jsx
@@ -92,8 +92,52 @@ const AboutPage = () => {
             helps identify Out of Memory (OOMKill) errors.
           </p>
           <div className="feature-btns">
-            <button className="feature-btn">Download for Mac Silicon</button>
-            <button className="feature-btn">Download for Mac Intel</button>
+            <a
+              href="../../../public/logo.png"
+              download={'Palaemon Installer MacOS Apple ARM'}
+            >
+              <button className="feature-btn">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    class="bi bi-apple"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
+                    <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
+                  </svg>
+                </div>
+                <div className="feature-btn-text">
+                  Download for MAC with Apple Chip
+                </div>
+              </button>
+            </a>
+            <a
+              href="../../../public/logo.png"
+              download={'Palaemon Installer MacOS Intel x86'}
+            >
+              <button className="feature-btn">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    class="bi bi-apple"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
+                    <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
+                  </svg>
+                </div>
+                <div className="feature-btn-text">
+                  Download for MAC with Intel Chip
+                </div>
+              </button>
+            </a>
           </div>
         </main>
         <figure className="feature-right-side">
@@ -179,7 +223,7 @@ const AboutPage = () => {
               </svg>
             </div>
             <p className="features-right-heading">
-              In Depth Analysis for OOMKilled Pods
+              Detailed Error Analysis for Pods
             </p>
             <p className="features-text">
               Filter through recent OOMKill events and diagnose potential causes
@@ -202,15 +246,15 @@ const AboutPage = () => {
       {/* -------------------- START OF TEAM BIO -------------------- */}
 
       <section id="contact">
-        <p>
-          This is where we'll have our beautiful photos and ways to connect with
-          such spectacular engineers like us!
-        </p>
         <div className="spectacular">
           <div className="spectacular-person">
             <div className="spectacular-person-photo">
               {' '}
-              <img src="public/Si-Young-Mah.png" alt="" />
+              <img
+                className="photos"
+                src="public/headshots/SiYoung-Mah.jpg"
+                alt=""
+              />
               {/* <img src="public/logo.png" alt="" /> */}
             </div>
             <div className="spectacular-person-name">Si Young Mah</div>
@@ -237,7 +281,11 @@ const AboutPage = () => {
           <div className="spectacular-person">
             <div className="spectacular-person-photo">
               {' '}
-              <img src="public/Patrick-Hu.png" alt="" />
+              <img
+                className="photos"
+                src="public/headshots/Patrick-Hu.jpeg"
+                alt=""
+              />
               {/* <img src="public/logo.png" alt="" /> */}
             </div>
             <div className="spectacular-person-name">Patrick Hu</div>
@@ -264,7 +312,11 @@ const AboutPage = () => {
           <div className="spectacular-person">
             <div className="spectacular-person-photo">
               {' '}
-              <img src="public/Thang-Thai.png" alt="" />
+              <img
+                className="photos"
+                src="public/headshots/Thang-Thai.jpg"
+                alt=""
+              />
               {/* <img src="public/logo.png" alt="" /> */}
             </div>
             <div className="spectacular-person-name">Thang Thai</div>
@@ -291,7 +343,11 @@ const AboutPage = () => {
           <div className="spectacular-person">
             <div className="spectacular-person-photo">
               {' '}
-              <img src="public/Raivyno-Sutrisno.jpg" alt="" />
+              <img
+                className="photos"
+                src="public/headshots/Raivyno-Sutrisno.jpg"
+                alt=""
+              />
             </div>
             <div className="spectacular-person-name">
               Raivyno (Lenny) Sutrisno
